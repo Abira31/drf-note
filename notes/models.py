8b837ec63988336e7af96e16b312ee44f0ff91bb
artur <tyktybaev_ad@okb2-tmn.ru>
2023-02-27 14:20:19 +0500

    create model Note

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['-update']
