3a40ba8a8f79c9bf194413e8ab4c64dc7c65050e
artur <tyktybaev_ad@okb2-tmn.ru>
2023-02-28 15:21:34 +0500

    update urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
 from api import views
 
+notes_list = views.NoteViewSet.as_view({
+    'get':'list',
+    'post':'list'
+})
+notes_detail = views.NoteViewSet.as_view({
+    'get':'retrieve',
+    'put':'update',
+    'patch':'partial_update',
+    'delete':'destroy'
+
+})
+
 urlpatterns = [
-    path('notes/',views.NoteListAPIView.as_view()),
-    path('notes/<int:pk>/',views.NoteDetailView.as_view(),
-         name=views.NoteDetailView.name),
+    # path('notes/',views.NoteViewSet.as_view()),
+    # path('notes/<int:pk>/',views.NoteDetailView.as_view(),
+    #      name=views.NoteDetailView.name),
+    path('notes/',notes_list,name='notes-list'),
+    path('notes/<int:pk>/',notes_detail,name='notes-detail'),
 ]
