b8133f69186f4e49f8791148fb3fca5551623a35
artur <tyktybaev_ad@okb2-tmn.ru>
2023-02-28 15:41:58 +0500

    update urls

--- api/urls.py
+++ api/urls.py
@@ -1,22 +1,30 @@
 from django.urls import path
 from api import views
+from rest_framework.routers import DefaultRouter
 
-notes_list = views.NoteViewSet.as_view({
-    'get':'list',
-    'post':'list'
-})
-notes_detail = views.NoteViewSet.as_view({
-    'get':'retrieve',
-    'put':'update',
-    'patch':'partial_update',
-    'delete':'destroy'
+# notes_list = views.NoteViewSet.as_view({
+#     'get':'list',
+#     'post':'list'
+# })
+# notes_detail = views.NoteViewSet.as_view({
+#     'get':'retrieve',
+#     'put':'update',
+#     'patch':'partial_update',
+#     'delete':'destroy'
+#
+# })
 
-})
+router = DefaultRouter()
+router.register('notes',views.NoteViewSet,basename='notes')
+
 
 urlpatterns = [
     # path('notes/',views.NoteViewSet.as_view()),
     # path('notes/<int:pk>/',views.NoteDetailView.as_view(),
     #      name=views.NoteDetailView.name),
-    path('notes/',notes_list,name='notes-list'),
-    path('notes/<int:pk>/',notes_detail,name='notes-detail'),
+
+    # path('notes/',notes_list,name='notes-list'),
+    # path('notes/<int:pk>/',notes_detail,name='notes-detail'),
 ]
+
+urlpatterns += router.urls
