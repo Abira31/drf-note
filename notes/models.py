c99aca54a5f8f0732b15b8f72418ce3ad4a700f9
artur <tyktybaev_ad@okb2-tmn.ru>
2023-02-28 16:27:14 +0500

    update model Note

--- notes/models.py
+++ notes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Note(models.Model):
-    author = models.ForeignKey(User,null=True,blank=True,on_delete=models.SET_NULL)
+    author = models.ForeignKey(User,null=True,blank=False,on_delete=models.SET_NULL)
     title = models.CharField(max_length=255)
     text = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
